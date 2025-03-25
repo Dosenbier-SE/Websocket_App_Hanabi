b67bc6ac21f0de4116f0422b68d974c8f141527d
Gabriel Ranftler <gabriel.ranftler@gmx.at>
2025-03-25 20:11:32 +0100

    added sonar cloud configs to build.gradle.kts / build.yml config

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("org.sonarqube") version "6.0.1.5171"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Dosenbier-SE_Websocket_App_Hanabi")
+        property("sonar.organization", "dosenbier-se")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+android {
+    namespace = "com.example.hanabi"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.hanabi"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
